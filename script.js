4c5dc1d2abd19a46ce787e77f766c81d3962d0fb
MatthewSidoti <coding.sidoti@gmail.com>
2024-07-08 18:50:50 -0400

    Pseudocode Submit

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// Create Board
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+  // specify the size/width/length of board grid/canvas size
+  //specify where the snake will start and what direction  it will start moving 
+  // randomize where the food will appear on the board
+  // let score start at 0
+
 /* As a user I want to see the word Snake in big letters*/
 
 /* I want to specify how big the areana/play area is */
@@ -7,25 +15,26 @@
 /* 
 As a user I want Game Over to pop up on the screen */
 
-/* As a user I want the high score to always be displayed even if restarting the game */
+/* NTH-As a user I want the high score to always be displayed even if restarting the game */
 
-/* If high score is beaten show "New High Score" on screen  */
+/* NTH-If high score is beaten show "New High Score" on screen  */
 
-/* If new high score is not beaten show "Better Luck Next Time" */
+/* NTH-If new high score is not beaten show "Better Luck Next Time" */
 
 /* As a user I want to also have a play button */
 
-/*  As a user I want the choice of difficulty to pop up one being easy medium or difficult. easy will be slower medium will be normal speed and difficult will be fast */
+/* NTH- As a user I want the choice of difficulty to pop up one being easy medium or difficult. easy will be slower medium will be normal speed and difficult will be fast */
 
 /* Create visual for buttons */
 
 /* Creat visual for the food/blocks that the snake eats */
 
-/* As a user I want the snack to grow by 1 block for every block/food it eats. Grow by 2 if it eats the bonus item.  */
+/* As a user I want the snack to grow by 1 block for every block/food it eats.
+//  NTH-Grow by 2 if it eats the bonus item.  */
 
 /* As a user I want a score to show and increase for every block that I eat by 50 points */
 
-/*  At random times I want a bonus item to show up woth 200 points. I want it to dissappear if not gotten in a certain amount of time */
+/*  NTH-At random times I want a bonus item to show up woth 200 points. I want it to dissappear if not gotten in a certain amount of time */
 
 
 /* As a user I want a restart button to pop up once I lose */
@@ -37,39 +46,43 @@ As a user I want Game Over to pop up on the screen */
 
 // Click play
 
-// Click  Easy
+// NTH-Click  Easy
 
-// Click Medium
+// NTH-Click Medium
 
-// Click Hard
+// NTH-Click Hard
 
 // Click Restart
 
 
 /*-------------- Constants -------------*/
-
-
+// specify the size/width/length of board grid/canvas size
+  //specify where the snake will start and what direction  it will start moving 
+  // let score start at 0
 /*---------- Variables (state) ---------*/
-
+// randomize where the food will appear on the board
 
 /*----- Cached Element References  -----*/
 
 
 /*-------------- Functions -------------*/
 
-// In JS I want to specify what buttons control what. I need an up,down,left,right controls.
-
-
-/*----------- Event Listeners ----------*/
-
 // In JS I need to create a click function for all my buttons
 
 // Click play
 
-// Click  Easy
+//NTH- Click  Easy
 
-// Click Medium
+// NTH-Click Medium
 
-// Click Hard
+// NTH-Click Hard
 
 // Click Restart
+
+
+/*----------- Event Listeners ----------*/
+
+// In JS I want to specify what buttons control what. I need an up,down,left,right controls.
+
+// Create a listener for when the snake hits the boarder or itself the game is over
+
